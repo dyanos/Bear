9c53b4e72b09049ec7b9ab29d3dfaf6146407963
dyanos <dyanos@gmail.com>
2014-03-12 22:05:38 +0900

    graph관련 코드 때문에 추가

---
+++ CSEL/Graph.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+class Graph:
+  def __init__(self, directional = False):
+    self.directional = directional
+    self.edges = {}
+    self.nodes = []
+
+  def _addEdge(self, source, destination):
+    if self.edges.has_key(source):
+      self.edges[source] |= set([destination])
+    else:
+      self.edges[source] = set([destination])
+
+  def addEdge(self, source, destination):
+    self._addEdge(source, destination)
+    if self.directional:
+      self._addEdge(destination, source)
+
+  def _isEmpty(self, s):
+    if len(s) == 0: return True
+    return False
+
+  def isConnected(self, nodeOne, nodeTwo):
+    if self.edges.has_key(nodeOne):
+      if not self._isEmpty(self.edges[nodeOne] & set([nodeTwo])):
+        return True
+
+    return False
